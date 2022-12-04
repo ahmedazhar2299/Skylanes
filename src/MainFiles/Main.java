31dee4a632fab645840f4ee157802a09e229529a
Muhammad Ahmed <62472263+ahmedazhar2299@users.noreply.github.com>
2022-12-04 17:48:16 +0500

    v1.0

---
+++ src/MainFiles/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MainFiles;
+import javax.swing.JFrame;
+import java.util.*;
+import GUI_ALMS.*;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   /* Customer_Login cl=new Customer_Login();
+    cl.setVisible(true);*/
+    
+    Admin_Login AL = new Admin_Login();
+    AL.setVisible(true);
+    
+    }
+           
+}
